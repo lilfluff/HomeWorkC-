85708a246aad6c3a4cf1938fbcb23d717a90b510
Anastasiia <aktlnkv@gmail.com>
2022-12-24 16:22:25 +0500

    Задача 4

--- ДЗ1/Program.cs
+++ ДЗ1/Program.cs
@@ -26,9 +26,21 @@
 
 // Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+// Console.WriteLine("Введите число");
+// int a = int.Parse(Console.ReadLine()!);
+// if (a%2==0)
+//     Console.WriteLine("Число чётное");
+// else 
+//     Console.WriteLine("Число нечётное");
+
+// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
 Console.WriteLine("Введите число");
-int a = int.Parse(Console.ReadLine()!);
-if (a%2==0)
-    Console.WriteLine("Число чётное");
-else 
-    Console.WriteLine("Число нечётное");
+int N = int.Parse(Console.ReadLine()!);
+int inter = 1;
+while (inter<=N) {
+    if (inter%2==0)
+        Console.Write($" {inter}");
+    inter++;
+}
+    
