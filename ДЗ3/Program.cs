0ee7d1b195798cd8a47948004b1ce6febb8d3350
Anastasiia <aktlnkv@gmail.com>
2023-01-08 23:12:07 +0500

    Все задачи работают

--- ДЗ3/Program.cs
+++ ДЗ3/Program.cs
@@ -1,8 +1,16 @@
-﻿Console.WriteLine("Введите пятизначное число: ");
-int num = int.Parse(Console.ReadLine()!);
-int num = Array[num];
-if (Array.Lenght == 5) {
- int a 
+﻿Console.WriteLine("Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
+Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine()!); 
+if (num>9999 && num<100000) {
+    string num1 = num.ToString();
+   if ((num1[0]==num1[4])||(num1[1]==num1[3]))
+    Console.WriteLine("Число является палиндромом");
+    else {
+        Console.WriteLine("Число не является палиндромом");
+    } 
+}
+else {
+    Console.WriteLine("Это не пятизначное число");
 }
 Console.WriteLine("Задача 2: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.");
 Console.WriteLine("Введите первую координату точки 1: ");
