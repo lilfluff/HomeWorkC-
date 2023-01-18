a0bd4b0abc93267dc7d7774b1405351306688d53
Anastasiia <aktlnkv@gmail.com>
2023-01-18 21:36:45 +0500

    работает задача 2 математически

--- ДЗ4/Program.cs
+++ ДЗ4/Program.cs
@@ -17,27 +17,30 @@
 // Console.WriteLine($"{result}");
 
 
-// Console.WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
-// Console.WriteLine("Введите число ");
-// int num = int.Parse(Console.ReadLine()!);
-// Console.WriteLine($"Сумма цифр в числе, которое Вы ввели {SumNums(num)}");
-// int SumNums (string num) {
-//    int[]array = num.Length;
-//    while (int i>0) {
-//     int []array
-//      int sum = array[0]+;
-//      i++; 
-//    }
-// }
+Console.WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе, которое Вы ввели {SumNums(num)}");
+
+int SumNums(int num) {
+    int sum = 0;
+    int i = 0;
+while (num > 0) {
+   sum = sum + num%10;
+   num = num/10;
+   i++;
+}
+return sum;
+}
 
 
-Console.WriteLine("Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
-int []Array(int size) {
-    int[]array = new int[size];
-    for (int i=0; i<size; i++){
-        array[i]=new Random().Next(9);
-    }
-    return array;
-    }
-int[]array = Array(8);
-Console.WriteLine($"[{String.Join(',', array)}]");
+// Console.WriteLine("Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
+// int []Array(int size) {
+//     int[]array = new int[size];
+//     for (int i=0; i<size; i++){
+//         array[i]=new Random().Next(9);
+//     }
+//     return array;
+//     }
+// int[]array = Array(8);
+// Console.WriteLine($"[{String.Join(',', array)}]");
