bf9be5f9a62fdd9fb33694ddf84ccf459a3364ff
Anastasiia <aktlnkv@gmail.com>
2023-01-18 23:35:08 +0500

    наконец работает 2 задача строково

--- ДЗ4/Program.cs
+++ ДЗ4/Program.cs
@@ -1,39 +1,39 @@
-﻿Console.WriteLine("Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
-Console.WriteLine("Введите число А:");
-int A = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число B:");
-int B = int.Parse(Console.ReadLine()!);
-int result = Result(A, B);
-Console.WriteLine($"Число {A} в степени {B} равно {result}");\
+﻿// Console.WriteLine("Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+// Console.WriteLine("Введите число А:");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число B:");
+// int B = int.Parse(Console.ReadLine()!);
+// int result = Result(A, B);
+// Console.WriteLine($"Число {A} в степени {B} равно {result}");\
 
-int Result(int a, int b)
-{
-    int i = 0;
-    int result = 1;
-    while (i < b)
-    {
-        result = result * a;
-        i++;
-    }
-    return result;
-}
+// int Result(int a, int b)
+// {
+//     int i = 0;
+//     int result = 1;
+//     while (i < b)
+//     {
+//         result = result * a;
+//         i++;
+//     }
+//     return result;
+// }
 
 
-Console.WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
-Console.WriteLine("Введите число ");
-int num = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Сумма цифр в числе, которое Вы ввели {SumNums(num)}");
+// Console.WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
+// Console.WriteLine("Введите число ");
+// int num = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Сумма цифр в числе, которое Вы ввели {SumNums(num)}");
 
-int SumNums(int num) {
-    int sum = 0;
-    int i = 0;
-while (num > 0) {
-   sum = sum + num%10;
-   num = num/10;
-   i++;
-}
-return sum;
-}
+// int SumNums(int num) {
+//     int sum = 0;
+//     int i = 0;
+// while (num > 0) {
+//    sum = sum + num%10;
+//    num = num/10;
+//    i++;
+// }
+// return sum;
+// }
 
 
 Console.WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
@@ -47,26 +47,25 @@ int SumNums(string num)
     int lenght = num.Length;
     int sum = 0;
     int i = 0;
-    int num = int.Parse(num);
     while (i < lenght)
     {
-        sum = sum + num[i];
+        sum = sum + int.Parse(num[i].ToString());
         i++;
     }
     return sum;
 }
 
 
-Console.WriteLine("Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
-int[] array = Array(8);
-Console.WriteLine($"[{String.Join(',', array)}]");
+// Console.WriteLine("Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
+// int[] array = Array(8);
+// Console.WriteLine($"[{String.Join(',', array)}]");
 
-int[] Array(int size)
-{
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next(9);
-    }
-    return array;
-}
+// int[] Array(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(9);
+//     }
+//     return array;
+// }
