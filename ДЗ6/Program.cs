215f971b153d2625327e01d0a3bd417c711ecebb
Anastasiia <aktlnkv@gmail.com>
2023-01-25 23:07:24 +0500

    Работает задача 1

---
+++ ДЗ6/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
+Console.WriteLine("Введите несколько чисел ");
+string m = Console.ReadLine()!;
+string[] nums = m.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int[] num = new int[nums.Length];
+for (int i = 0; i < nums.Length; i++)
+{
+    num[i] = int.Parse(nums[i]);
+}
+Console.WriteLine($"Вы ввели {CountEl(num)} чисел > 0");
+
+int CountEl(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
